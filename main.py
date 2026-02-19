fd57cbcd81ccf4c6968573321615d9dc93349b1e
ShivamKR12 <sonya.shree.69@gmail.com>
2026-02-20 01:04:01 +0530

    Added a timer, score counter and a delay before the game start

--- main.py
+++ main.py
@@ -43,9 +43,12 @@ PLAYER_SCORE = 0
 OPPONENT_SCORE = 0
 game_font = pygame.font.Font(None, 32)
 
+# score timer variable
+score_time = True
+
 
 def ball_animation():
-    global BALL_SPEED_X, BALL_SPEED_Y, PLAYER_SCORE, OPPONENT_SCORE
+    global BALL_SPEED_X, BALL_SPEED_Y, PLAYER_SCORE, OPPONENT_SCORE, score_time
 
     # move the ball to the right each frame, based on BALL_SPEED_X and BALL_SPEED_Y
     ball.x += BALL_SPEED_X
@@ -60,11 +63,13 @@ def ball_animation():
     if ball.left <= 0:
         # if the ball hit's the left side of screen, then increase the player's score
         PLAYER_SCORE += 1
-        ball_restart()
+        # update the score time
+        score_time = pygame.time.get_ticks()
     if ball.right >= SCREEN_WIDTH:
         # if the ball hit's the right side of screen, then increase the opponent's score
         OPPONENT_SCORE += 1
-        ball_restart()
+        # update the score time
+        score_time = pygame.time.get_ticks()
     
     # check if the ball is colliding with any of the paddles or not
     # if it is, then flip the direction of the motion of the ball
@@ -91,12 +96,39 @@ def opponent_ai():
         opponent.bottom = SCREEN_HEIGHT
 
 def ball_restart():
-    global BALL_SPEED_X, BALL_SPEED_Y
+    global BALL_SPEED_X, BALL_SPEED_Y, score_time
+
+    # the current_time variable, unlike the score_time variable get's updated each frame
+    current_time = pygame.time.get_ticks()
+
     # move the ball of the center of the screen
     ball.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    # choose between going left or right when the ball resets
-    BALL_SPEED_Y *= random.choice((-1, 1))
-    BALL_SPEED_X *= random.choice((-1, 1))
+
+    # when the time difference between current_time and the score_time is less than 700 milliseconds, 
+    # then display the number 3 on the screen
+    if current_time - score_time < 700:
+        number_three = game_font.render('3', False, LIGHT_GREY)
+        screen.blit(number_three, (SCREEN_WIDTH / 2 - 10, SCREEN_HEIGHT / 2 + 20))
+    # when the difference is in between 700 milliseconds and 1.4 seconds, 
+    # display the number 2 
+    if 700 < current_time - score_time < 1400:
+        number_two = game_font.render('2', False, LIGHT_GREY)
+        screen.blit(number_two, (SCREEN_WIDTH / 2 - 10, SCREEN_HEIGHT / 2 + 20))
+    # and when it is greater than 1.4 seconds and less than 2.1 seconds, 
+    # display the number 1
+    if 1400 < current_time - score_time < 2100:
+        number_one = game_font.render('1', False, LIGHT_GREY)
+        screen.blit(number_one, (SCREEN_WIDTH / 2 - 10, SCREEN_HEIGHT / 2 + 20))
+
+    # we need to check if the difference between the score_time and the current_time is less than 2.1 sec
+    if current_time - score_time < 2100:
+        # if it is less than 2.1 sec, then the ball isn't moving
+        BALL_SPEED_X, BALL_SPEED_Y = 0, 0
+    else:
+        # otherwise the ball move in either of left or right direction randomly
+        BALL_SPEED_Y = 7 * random.choice((-1, 1))
+        BALL_SPEED_X = 7 * random.choice((-1, 1))
+        score_time = None
 
 
 # create a while loop to run the game
@@ -158,6 +190,11 @@ while True:
     # 4. a tuple for the end point ( half of the screen's width for the middle and 
     # half of the screen's height for the middle of the bottom of the window )
     pygame.draw.aaline(screen, LIGHT_GREY, (SCREEN_WIDTH / 2, 0), (SCREEN_WIDTH / 2, SCREEN_HEIGHT))
+
+    # when the score_time variable's type changes to or becames True, 
+    # the below statement becomes true and the ball_restart() function is called
+    if score_time:
+        ball_restart()
 
     # create the text to be displayed on the screen
     player_text = game_font.render(f'{PLAYER_SCORE}', False, LIGHT_GREY)
